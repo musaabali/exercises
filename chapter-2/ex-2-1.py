e8f843b4702e4e0da72b1d665be7c085d44de1f6
Musaab Ali <musaabali56@gmail.com>
2017-09-19 13:17:46 +0000

    Address program

--- chapter-2/ex-2-1.py
+++ chapter-2/ex-2-1.py
@@ -6,19 +6,19 @@
 # It will display the name and contact information for a teacher.
 
 # display the full name
-
+print("Musaab Ali")
 
 # display the address
-
+print("The white house down the street")
 
 # display the city, state and zip
-
+print("Coralville, IA, 52241")
 
 # display the phone number
-
+print("319-123-4567")
 
 # display the course area
-
+print("CIS-121")
 
 
 
